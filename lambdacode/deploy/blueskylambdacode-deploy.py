d80d49e023eb8a75a21c611301917b088acd6e3b
Gcladera <graucladera@autonoma.cat>
2026-04-25 14:04:59 +0200

    Lambda code final

--- lambdacode/deploy/blueskylambdacode-deploy.py
+++ lambdacode/deploy/blueskylambdacode-deploy.py
@@ -24,66 +24,46 @@ def get_posts(search_attributes, client):
         raise
 
 def get_relationships(client: Client, actors: list):
-    try:
-        social_graph = []
+    social_graph = []
 
-        for actor in actors:
-            try:
-                profile = client.get_profile(actor=actor)
-                
-                followers_dids = []
-                cursor = None
-                while True:
-                    res = client.get_followers(actor=actor, cursor=cursor)
-                    followers_dids.extend([(f.did, '0') for f in res.followers])
-                    cursor = res.cursor
-                    if not cursor:
-                        break
+    for actor in actors:
+        try:
+            profile = client.get_profile(actor=actor)
 
-                follows_dids = []
-                cursor = None
-                while True:
-                    res = client.get_follows(actor=actor, cursor=cursor)
-                    follows_dids.extend([(f.did, '1') for f in res.follows])
-                    cursor = res.cursor
-                    if not cursor:
-                        break
-
-                f = followers_dids + follows_dids
-                social_graph.append( {
-                    'id': hashlib.md5(actor.encode()).hexdigest(),
-                    'display_name': profile.display_name,
-                    'follows': f,
-                    'author_did': profile.did,
-                    'author_handle': profile.handle,
-                    }
-                )
-            except Exception as e:
-                logger.error(f"Error processing actor {actor}: {e}")
-                social_graph.append(None)
-
-        return social_graph
-    except Exception as e:
-        logger.error(f"Error in get_relationships: {e}")
-        raise
-
-def get_crypto_data():
-    try:
-        client = initialise_cryptos_api_client()
-        coins = client.coins.markets.get(vs_currency='eur', order='market_cap_desc', per_page=20, page=1)
-        coins_list = []
-        for i in range(len(coins)):
-            if i == len(coins)-1:
-             search_attributes = f"{coins[i].name} OR {coins[i].symbol}"
-            else:   
-                search_attributes = f"{coins[i].name} OR {coins[i].symbol} OR"
+            followers_dids = []
+            cursor = None
             
-            coins_list.append(search_attributes)
-        coins_list = " ".join(coins_list)
-        return coins_list
-    except Exception as e:
-        logger.error(f"Error in get_crypto_data: {e}")
-        raise
+            while True:
+                res = client.get_followers(actor=actor, cursor=cursor)
+                followers_dids.extend([(f.did, '0') for f in res.followers])
+                cursor = res.cursor
+                if not cursor:
+                    break
+
+            follows_dids = []
+            cursor = None
+
+            while True:
+                res = client.get_follows(actor=actor, cursor=cursor)
+                follows_dids.extend([(f.did, '1') for f in res.follows])
+                cursor = res.cursor
+                if not cursor:
+                    break
+
+            f = followers_dids + follows_dids
+            social_graph.append( {
+                'id': hashlib.md5(actor.encode()).hexdigest(),
+                'display_name': profile.display_name,
+                'follows': f,
+                'author_did': profile.did,
+                'author_handle': profile.handle,
+                }
+            )
+        except Exception as e:
+            logger.error(f"Error processing actor {actor}: {e}")
+            social_graph.append(None)
+
+    return social_graph
 
 def initialise_cryptos_api_client():
     try:
@@ -96,6 +76,25 @@ def initialise_cryptos_api_client():
         logger.error(f"Error initializing cryptos API client: {e}")
         raise
 
+def get_crypto_data(client):
+    try:
+        coins = client.coins.markets.get(vs_currency='eur', order='market_cap_desc', per_page=20, page=1)
+    except Exception as e:
+        logger.error(f"Error in get_crypto_data: {e}")
+        raise
+
+    coins_list = []
+    for i in range(len(coins)):
+        if i == len(coins)-1:
+            search_attributes = f"{coins[i].name} OR {coins[i].symbol}"
+        else:   
+            search_attributes = f"{coins[i].name} OR {coins[i].symbol} OR"
+        
+        coins_list.append(search_attributes)
+    coins_list = " ".join(coins_list)
+    return coins_list
+   
+
 def initialise_posts_api_client():
     try:
         client = Client()
@@ -106,15 +105,12 @@ def initialise_posts_api_client():
         raise
 
 def transform_data_to_parquet(data):
-    try:
-        df = pd.DataFrame(data)
-        buffer = io.BytesIO()
-        df.to_parquet(buffer, index=False, engine="pyarrow", coerce_timestamps='us', allow_truncated_timestamps=True)
-        buffer.seek(0)
-        return df, buffer
-    except Exception as e:
-        logger.error(f"Error in transform_data_to_parquet: {e}")
-        raise
+    df = pd.DataFrame(data)
+    buffer = io.BytesIO()
+    df.to_parquet(buffer, index=False, engine="pyarrow", coerce_timestamps='us', allow_truncated_timestamps=True)
+    buffer.seek(0)
+    return df, buffer
+
 
 def get_latests_posts(client, search_attributes):
     try:
@@ -133,35 +129,33 @@ def get_latests_posts(client, search_attributes):
         raise
 
 def process_posts_data(data):
-    try:
-        processed_data = []
-        for post in data:
-            if post[0] == 'posts':
-                for postview in post[1]:
-                    entry = {
-                        'id': hashlib.md5(postview.cid.encode()).hexdigest(),
-                        'cid': postview.cid, #content identifier
-                        'author_handle': postview.author.handle,
-                        'author_did': postview.author.did,
-                        'text': postview.record.text,
-                        'created_at': pd.to_datetime(postview.record.created_at),
-                        'indexed_at': pd.to_datetime(postview.indexed_at),
-                        'like_count': postview.like_count,
-                        'reply_count': postview.reply_count,
-                        'repost_count': postview.repost_count,
-                        'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    }
-                    processed_data.append(entry)
-        return processed_data
-    except Exception as e:
-        logger.error(f"Error in process_posts_data: {e}")
-        raise
+    processed_data = []
+    for post in data:
+        if post[0] == 'posts':
+            for postview in post[1]:
+                entry = {
+                    'id': hashlib.md5(postview.cid.encode()).hexdigest(),
+                    'cid': postview.cid, #content identifier
+                    'author_handle': postview.author.handle,
+                    'author_did': postview.author.did,
+                    'text': postview.record.text,
+                    'created_at': pd.to_datetime(postview.record.created_at),
+                    'indexed_at': pd.to_datetime(postview.indexed_at),
+                    'like_count': postview.like_count,
+                    'reply_count': postview.reply_count,
+                    'repost_count': postview.repost_count,
+                    'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                }
+                processed_data.append(entry)
+    return processed_data
+  
 
 def lambda_handler(event, context):
     try:
-        client = initialise_posts_api_client()
-        search_attributes = get_crypto_data()
-        data = get_posts(search_attributes=search_attributes, client=client)
+        client_posts = initialise_posts_api_client()
+        client_crypto = initialise_cryptos_api_client()
+        search_attributes = get_crypto_data(client=client_crypto)
+        data = get_posts(search_attributes=search_attributes, client=client_posts)
         cleaned_data = process_posts_data(data)
         accounts = set([post.author.did for post in data.posts])        
 
@@ -172,26 +166,25 @@ def lambda_handler(event, context):
 
         _, buffer_posts = transform_data_to_parquet(cleaned_data)
         
-        relationships = get_relationships(client=client, actors=accounts)
+        relationships = get_relationships(client=client_posts, actors=accounts)
         _, buffer_relationships = transform_data_to_parquet(relationships)
 
 
         s3 = boto3.client('s3')
         s3.upload_fileobj(
             Bucket='amzn-s3-tfgdl',
-            Key=f'bronze/posts/year={year}/month={month}/day={day}/posts_{timestamp}.parquet',
+            Key=f'bronze/posts/post_content/year={year}/month={month}/day={day}/posts_{timestamp}.parquet',
             Fileobj=buffer_posts
             )
 
         s3.upload_fileobj(
             Bucket='amzn-s3-tfgdl',
-            Key=f'bronze/social_media_relationships/year={year}/month={month}/day={day}/relationships_{timestamp}.parquet',
+            Key=f'bronze/posts/social_media_relationships/year={year}/month={month}/day={day}/relationships_{timestamp}.parquet',
             Fileobj=buffer_relationships
             )
     
         log_data = {
             "message": "Processant compte de Bluesky",
-            "actor": "usuario.bsky.social",
             "status": "success",
             "custom_metric": 42
         }
@@ -199,7 +192,7 @@ def lambda_handler(event, context):
         logger.info(json.dumps(log_data))
 
     except Exception as e:
-        logger.error(f"Error in lambda_handler: {e}\nAccount: usuario.bsky.social")
+        logger.error(f"Error in lambda_handler: {e}")
         raise
 
 lambda_handler(None, None)
--- lambdacode/deploy/geeckolambdacode-deploy.py
+++ lambdacode/deploy/geeckolambdacode-deploy.py
@@ -1,68 +1,162 @@
+import logging
 import os
 import io
 from coingecko_sdk import Coingecko
 import boto3
-from datetime import datetime, timezone
+from datetime import datetime
+import time
 import pandas as pd
-import pickle 
-import uuid
 from dotenv import load_dotenv
+import hashlib
+import json
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 load_dotenv(dotenv_path="/mnt/windows/Users/user/Documents/PERSONAL/Grau/Educacion/UAB/Any4/TFG/Codi/.env")
 
-def get_data(call_api=False):
-    if call_api:
-        client = initialise_api_client()
-        coins = client.coins.markets.get(vs_currency='eur', order='market_cap_desc', per_page=10, page=1)
-        # with open('coins.pkl', 'wb') as f:
-        #     pickle.dump(coins, f)
-        return coins
-    else:
-        with open('coins.pkl', 'rb') as f:
-            data = pickle.load(f)
-        return data
+def hash_row(row):
+    row_str = '|'.join(str(x) for x in row.values)
+    return hashlib.md5(row_str.encode()).hexdigest()
 
+def get_market_data(client):
+    try:
+        coins = client.coins.markets.get(vs_currency='eur', order='market_cap_desc', per_page=20, page=1)
+    except Exception as e:
+        logger.error(f"Error in get_market_data: {e}")
+        raise
+    df = pd.DataFrame([vars(item) for item in coins])
 
-def initialise_api_client():
-    client = Coingecko(
-        demo_api_key = os.getenv("COINGECKO_API_KEY"),
-        environment="demo",)
-    
-    return client
-
-def lambda_handler(event, context):
-    data = get_data(call_api=True)
-
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M')
-    year = datetime.now().strftime('%Y')
-    month = datetime.now().strftime('%m')
-    day = datetime.now().strftime('%d')
-    df = pd.DataFrame([vars(item) for item in data])
-
-    #Columna roi da problemas. En la mayoria de monedas es None
     df['roi_currency'] = df['roi'].apply(lambda x: x.currency if x else None)
     df['roi_percentage'] = df['roi'].apply(lambda x: x.percentage if x else None)
     df['roi_times'] = df['roi'].apply(lambda x: x.times if x else None)
     df = df.drop(columns=['roi'])
-    df['id'] = [str(uuid.uuid4()) for _ in range(len(df))]
+    df['row_id'] = df.apply(hash_row, axis=1)    
     df['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
-    #df.to_parquet(f'data/crypto/crypto_{timestamp}.parquet')
 
-    #s3 = boto3.client('s3')
-    #s3.upload_file(f'data/crypto/crypto_{timestamp}.parquet', 'amzn-s3-tfgdl', f'crypto/{year}/{month}/{day}/crypto_{timestamp}.parquet'
+    return df
     
+
+def get_trending_data(client):
+    try:
+        trending = client.search.trending.get()    
+    except Exception as e:
+        logger.error(f"Error getting trending data: {e}")
+        raise
+
+    categories_list = []
+    for cat in trending.categories:
+        categories_list.append({
+            'category_id': cat.id,
+            'name': cat.name,
+            'coins_count': cat.coins_count,
+            'market_cap_usd': cat.data.market_cap,
+            'volume_24h_usd': cat.data.total_volume,
+            'change_24h_usd': cat.data.market_cap_change_percentage_24h.usd,
+            'change_1h_usd': cat.market_cap_1h_change,
+            'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        })
+
+    df_categories = pd.DataFrame(categories_list)
+    return df_categories
+
+def get_community_sentiment(client, coin_ids):
+    sentiment_list = []
+    for cid in coin_ids:
+        try:
+            coin_data = client.coins.get_id(
+                id=cid, 
+                localization=False, 
+                tickers=False, 
+                market_data=False, 
+                developer_data=False, 
+            )
+        except Exception as e:
+            logger.warning(f"Error fetching sentiment for {cid}: {e}")
+            
+        coin_dict = vars(coin_data) if hasattr(coin_data, '__dict__') else coin_data #Converteix un objecte a dict si no ho és
+
+        sentiment_list.append({
+            'coin_id': cid,
+            'sentiment_up_percentage': coin_dict.get('sentiment_votes_up_percentage'),
+            'sentiment_down_percentage': coin_dict.get('sentiment_votes_down_percentage'),
+            'watchlist_count': coin_dict.get('watchlist_portfolio_users'),
+            'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        })
+        
+        time.sleep(1.5) #per no s'aturar l'API
+        
+    
+    df_sentiment = pd.DataFrame(sentiment_list) 
+    return df_sentiment
+
+def initialise_api_client():
+    try:
+        client = Coingecko(
+            demo_api_key = os.getenv("COINGECKO_API_KEY"),
+            environment="demo",)
+        return client
+    except Exception as e:
+        logger.error(f"Error in initialise_api_client: {e}")
+        raise
+
+def convert_df_to_parquet(df):
     buffer = io.BytesIO()
     df.to_parquet(buffer, index=False, engine="pyarrow", coerce_timestamps='us', allow_truncated_timestamps=True)
     buffer.seek(0)
+    return buffer
+  
 
-    s3 = boto3.client('s3')
-    s3.put_object(
-        Bucket='amzn-s3-tfgdl',
-        Key=f'bronze/crypto/year={year}/month={month}/day={day}/crypto_{timestamp}.parquet',
-        Body=buffer.getvalue(),
-        ContentType="application/x-parquet"
+def lambda_handler(event, context):
+    try:
+        client = initialise_api_client()
+        top_20_df = get_market_data(client)
+        coin_ids = top_20_df['id'].tolist()
+        sentiment_data = get_community_sentiment(client, coin_ids)
+        trending_data = get_trending_data(client) 
+        print(sentiment_data)
+        print('-----------------------------')
+        print(trending_data)
+
+        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M')
+        year = datetime.now().strftime('%Y')
+        month = datetime.now().strftime('%m')
+        day = datetime.now().strftime('%d')
+        
+        
+        top_20_parquet = convert_df_to_parquet(top_20_df)
+        sentiment_parquet = convert_df_to_parquet(sentiment_data)
+        trending_parquet = convert_df_to_parquet(trending_data)
+
+        s3 = boto3.client('s3')
+
+        s3.upload_fileobj(
+            Bucket='amzn-s3-tfgdl',
+            Key=f'bronze/crypto/market_ranking/year={year}/month={month}/day={day}/crypto_{timestamp}.parquet',
+            Fileobj=top_20_parquet
         )
-    
 
+        s3.upload_fileobj(
+            Bucket='amzn-s3-tfgdl',
+            Key=f'bronze/crypto/sentiment/year={year}/month={month}/day={day}/sentiment_{timestamp}.parquet',
+            Fileobj=sentiment_parquet
+        )
 
+        s3.upload_fileobj(
+                Bucket='amzn-s3-tfgdl',
+                Key=f'bronze/crypto/trending/year={year}/month={month}/day={day}/trending_{timestamp}.parquet',
+                Fileobj=trending_parquet
+                )
+        
+        log_data = {
+            "message": "Processant compte de CoinGecko",
+            "status": "success",
+            "custom_metric": 42
+        }
+        
+        logger.info(json.dumps(log_data))
+
+    except Exception as e:
+        logger.error(f"Error in lambda_handler: {e}")
+        raise
+
+lambda_handler(None, None)
